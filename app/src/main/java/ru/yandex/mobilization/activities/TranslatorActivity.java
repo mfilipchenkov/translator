6c87a6c23468437bfee960caa685089a2dec871c
Mike Filipchenkov <m.filipchenkov@bars-open.ru>
2017-04-01 16:58:19 +0300

    init commit

---
+++ app/src/main/java/ru/yandex/mobilization/activities/TranslatorActivity.java
@@ -0,0 +1,15 @@
+package ru.yandex.mobilization.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ru.yandex.mobilization.R;
+
+public class TranslatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_translator);
+    }
+}
